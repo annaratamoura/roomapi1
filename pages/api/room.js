fdc335100b105f4bd54ec7ff9146e16c0b70fad3
annaratamoura <annamoura@outlook.com>
2025-07-29 10:56:03 +1200

    Update room.js

--- pages/api/room.js
+++ pages/api/room.js
@@ -1,25 +1,22 @@
 export default async function handler(req, res) {
-  const { id } = req.query;
+  const { id } = req.query
 
   if (!id) {
-    return res.status(400).json({ error: "Missing ID" });
+    return res.status(400).json({ error: "Missing room ID" })
   }
 
   try {
     const response = await fetch(
       `https://api.sheetbest.com/sheets/9d96c8f3-19d6-4d09-84f3-92bfdaca7f47/search?id=${id}`
-    );
-    const data = await response.json();
+    )
+    const data = await response.json()
 
-    if (!data || data.length === 0) {
-      return res.status(404).json({ stock: 0 });
+    if (data.length === 0) {
+      return res.status(404).json({ error: "Room not found" })
     }
 
-    res.status(200).json({
-      stock: Number(data[0].stock),
-      stripe_link: data[0].stripe_link,
-    });
+    return res.status(200).json(data[0])
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: "Failed to fetch data" })
   }
 }
